26d27bb223d360d044f21e5881de3fb18eeb7452
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-04-14 17:32:39 -0300

    Desafio Concluído

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,31 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type must be income or outcome.');
+    }
+
+    if (typeof value !== 'number') {
+      throw Error('Value must be a number.');
+    }
+
+    if (typeof title !== 'string') {
+      throw Error('Title must be a string.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error("You don't have enought balance.");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
